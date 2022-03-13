6b8073829778c2c427a2b0c3bfc057838fc92700
Subhabrata Tripathy <42057906+DeepMake@users.noreply.github.com>
2022-03-13 13:35:33 +0530

    Add files via upload

---
+++ A to Z Javascript/Function/scope.js
@@ -0,0 +1,30 @@
+// Global scope : it can access in all part of program
+/*
+const name  = 'Jhon';
+
+const logName = () =>{
+  console.log(name);
+}
+
+logName()
+
+// Local Scope/ function scope
+
+const fun = () =>{
+  console.log()
+
+}
+
+//Block Scope
+if(true){
+  const name = 'jhonny';
+  console.log(name);
+}
+console.log(name);
+*/
+
+//Hosting in Javascript (varible declear always in top otherwise hoisting happen)
+var age;
+console.log( age);
+age=20;
+ // Hoistosting also happen with function 
---
+++ A to Z Javascript/Function/script.js
@@ -0,0 +1,52 @@
+// Function (a block f code ==> performs a task)
+
+// A fuction decleartion(defineing a function)
+
+// function square(number){
+//   return number * number; // every function have return value
+// }
+
+// // A funtion call(invoking/calling/executive a function)
+// const result = square(5);// here we store the return data from the function into result variable
+// console.log(result);
+
+
+// // A function decleation
+// function name(params){
+//   //statements
+// }
+
+// // A function expression
+// const name = function(params){
+//   //statements
+// }
+
+// // An arrow function
+// const name = (params) =>{
+//   //statements
+// }
+
+
+// function sayHi(name){
+//   console.log(`Hi, ${name}`);
+// }
+
+// sayHi('Joe');
+// sayHi('Jhon');
+
+// Returned Undefined 
+// function add (a, b){
+//   return a+b;
+// }
+
+// const sum = add(2, 2);
+// console.log(sum);
+
+// exapmle of arrow function
+const square = (x) =>{
+  return x*x;
+}
+const y= square(4);
+console.log(y);
+
+// const fun = c => c*c; 
