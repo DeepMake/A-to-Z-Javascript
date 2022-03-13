444f56a9df9c09c79cb2576a70ed24c271eb0621
Subhabrata Tripathy <42057906+DeepMake@users.noreply.github.com>
2022-03-13 13:36:01 +0530

    Add files via upload

---
+++ A to Z Javascript/length/script.js
@@ -0,0 +1,81 @@
+/*
+const name = 'Tripathy';
+
+console.log(name);
+console.log(name[5]);
+
+
+// how to print last letter
+
+console.log(name[name.length-1]);
+
+
+// chane the charcter case of string
+
+const char = 'Hello , How are You?';
+
+const lchar = char.toLowerCase();
+const uchar = char.toUpperCase();
+console.log(lchar);
+console.log(uchar);
+
+
+
+// Searching for a substring
+
+const hobbies ='I Love HTML,CSS and JavaScript.';
+
+//1. Indexof()
+const index = hobbies.indexOf('JavaScript');
+console.log(index);
+
+//includes() - boolean values(true or false)
+const inc = hobbies.includes('JavaScript');
+console.log(inc);
+
+//sartsWith() - boolean values(true or false)
+
+//endsWith() - boolean values(true or false)
+
+
+//-- Getting for a substring
+
+const example = 'hotdog';
+
+//slice()
+const dog = example.slice(3,6);
+const hot = example.slice(0,3);
+console.log(dog);
+console.log(hot);
+
+
+//Split a string
+
+const a ='dog';
+console.log (a.split(''));
+
+const b ='I Love HTML,CSS and JavaScript.';
+console.log (b.split(' '));
+
+
+
+//reverse a string
+
+const aa ='I Love JavaScript';
+
+const bb = aa.split('').reverse().join();
+console.log (bb);
+
+//repeat()
+
+const dogsays = 'woof';
+console.log(dogsays.repeat(5));
+
+*/
+
+// trim()
+
+const string = '   Hello World!  ';
+console.log(string.trim( ));
+
+
